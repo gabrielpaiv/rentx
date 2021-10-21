49635a924ecaf15ea127db9d11d90deb1cc8dc22
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-10-21 13:18:40 -0300

    Added Calendar component Added Scheduling Screen

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import {
 
 import { ThemeProvider } from 'styled-components'
 
-import { CarDetails } from './src/screens/CarDetails'
+import { Scheduling } from './src/screens/Scheduling'
 import AppLoading from 'expo-app-loading'
 import theme from './src/styles/theme'
 
@@ -29,7 +29,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   )
 }
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useTheme } from 'styled-components'
+import { BackButton } from '../../components/BackButton'
+
+import {
+  Header,
+  Container,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer
+} from './styles'
+
+import ArrowSvg from '../../assets/arrow.svg'
+import { StatusBar } from 'react-native'
+import { Button } from '../../components/Button'
+import { Calendar } from '../../components/Calendar'
+
+export function Scheduling() {
+  const theme = useTheme()
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+        <Title>
+          Escolha uma {'\n'}data de início e {'\n'}fim do aluguel
+        </Title>
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={true}>21/10/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>21/10/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+      <Content>
+        <Calendar />
+      </Content>
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  )
+}
